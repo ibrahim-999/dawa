f08895580c5bf4261fa586e35a868cf0a057e89e
unknown <aaaahmed91@gmail.com>
2023-06-09 12:11:10 +0300

    add firebase

--- public/admin-panel-assets/v1/js/firebase.js
+++ public/admin-panel-assets/v1/js/firebase.js
@@ -1,5 +1,5 @@
-     var firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
+var firebaseConfig = {
+    apiKey: "AAAAAVnOUgs:APA91bERqVmF7tT6G9T-65kRgiHwmpdz2Vl6hdbz0cWc9m3HybKwofoz3UxI7lKaGIBccYxfIVvcrHkn3bT3azIfZugpdtYaFhV-X71F84LN2qfDLRyYVZHOJa6ERayE1V9gNw6U_s1Z",
     authDomain: "YOUR_AUTH_DOMAIN",
     databaseURL: "YOUR_DATABASE_URL",
     projectId: "YOUR_PROJECT_ID",
@@ -7,9 +7,9 @@
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
 };
-    firebase.initializeApp(firebaseConfig);
-     var messagesRef = firebase.database().ref('messages');
-     messagesRef.on('child_added', function(snapshot) {
-         var message = snapshot.val();
-         console.log('New message:', message);
-     });
+firebase.initializeApp(firebaseConfig);
+var messagesRef = firebase.database().ref('messages');
+messagesRef.on('child_added', function (snapshot) {
+    var message = snapshot.val();
+    console.log('New message:', message);
+});
