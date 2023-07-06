b5a6927d54282856a71c5bf23d666c58f22ddcf2
unknown <aaaahmed91@gmail.com>
2023-07-06 10:08:22 +0300

    add

--- app/Console/Commands/CampaignCommend.php
+++ app/Console/Commands/CampaignCommend.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use App\Jobs\SendCampaignNotificationJob;
 use App\Models\CampNotification;
+use App\Models\User;
+use App\Models\Vendor;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 
@@ -28,23 +30,36 @@ class CampaignCommend extends Command
      */
     public function handle(): void
     {
+        $customers = User::all();
+
+        $vendors = Vendor::all();
+
         $daily_notifications = CampNotification::where('schedule_type', 1)->get();
 
         $weekly_notifications = CampNotification::where('schedule_type', 2)->get();
 
+
         if ($daily_notifications->count()) {
             foreach ($daily_notifications as $daily_notification) {
+                if ($daily_notifications->user_type != 1) {
+                    $customers = $daily_notification->customers;
+                    $vendors = $daily_notification->vendors;
+                }
                 if (Carbon::parse($daily_notification->start_date)->gte(Carbon::now()) && Carbon::parse($daily_notification->end_date)->lte(Carbon::now())) {
-                    SendCampaignNotificationJob::dispatch($daily_notification->customers, $daily_notification->vendors, $daily_notification);
+                    SendCampaignNotificationJob::dispatch($customers, $vendors, $daily_notification);
                 }
             }
         }
 
         if ($weekly_notifications->count()) {
             foreach ($weekly_notifications as $weekly_notification) {
+                if ($daily_notifications->user_type != 1) {
+                    $customers = $weekly_notification->customers;
+                    $vendors = $weekly_notification->vendors;
+                }
                 if (date('D') == __('text.days_' . $weekly_notification->days_of_week)) {
                     if (Carbon::parse($weekly_notification->start_date)->gte(Carbon::now()) && Carbon::parse($weekly_notification->end_date)->lte(Carbon::now())) {
-                        SendCampaignNotificationJob::dispatch($weekly_notification->customers, $weekly_notification->vendors, $weekly_notification);
+                        SendCampaignNotificationJob::dispatch($customers, $vendors, $weekly_notification);
                     }
                 }
             }
