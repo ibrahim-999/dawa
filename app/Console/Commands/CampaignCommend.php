d865948e50d8d263e0ae98cc7a126bcd0d97416a
unknown <aaaahmed91@gmail.com>
2023-07-07 10:44:28 +0300

    fixed

--- app/Console/Commands/CampaignCommend.php
+++ app/Console/Commands/CampaignCommend.php
@@ -42,8 +42,8 @@ class CampaignCommend extends Command
         if ($daily_notifications->count()) {
             foreach ($daily_notifications as $daily_notification) {
                 if ($daily_notifications->user_type != 1) {
-                    $customers = $daily_notification->customers;
-                    $vendors = $daily_notification->vendors;
+                    $customers = $daily_notification->user_type == 2 ? $daily_notification->customers : [];
+                    $vendors = $daily_notification->user_type == 3 ? $daily_notification->vendors : [];
                 }
                 if (Carbon::parse($daily_notification->start_date)->gte(Carbon::now()) && Carbon::parse($daily_notification->end_date)->lte(Carbon::now())) {
                     SendCampaignNotificationJob::dispatch($customers, $vendors, $daily_notification);
@@ -53,9 +53,9 @@ class CampaignCommend extends Command
 
         if ($weekly_notifications->count()) {
             foreach ($weekly_notifications as $weekly_notification) {
-                if ($daily_notifications->user_type != 1) {
-                    $customers = $weekly_notification->customers;
-                    $vendors = $weekly_notification->vendors;
+                if ($weekly_notification->user_type != 1) {
+                    $customers = $weekly_notification->user_type == 2 ? $weekly_notification->customers : [];
+                    $vendors = $weekly_notification->user_type == 3 ? $weekly_notification->vendors : [];
                 }
                 if (date('D') == __('text.days_' . $weekly_notification->days_of_week)) {
                     if (Carbon::parse($weekly_notification->start_date)->gte(Carbon::now()) && Carbon::parse($weekly_notification->end_date)->lte(Carbon::now())) {
